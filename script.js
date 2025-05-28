907ec3cc343f75ecd4459fa4009a5a274e0f436a
estfueort <you@example.com>
2025-05-29 00:56:14 +0200

    index casi terminado

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ document.querySelector('.search-form').addEventListener('submit', function(e) {
   }
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const banner = document.getElementById('cookie-banner');
+    const acceptBtn = document.getElementById('accept-cookies');
+
+    if (!localStorage.getItem('cookiesAccepted')) {
+      banner.style.display = 'flex';
+    }
+
+    acceptBtn.addEventListener('click', function () {
+      localStorage.setItem('cookiesAccepted', 'true');
+      banner.style.display = 'none';
+    });
+  });
+
